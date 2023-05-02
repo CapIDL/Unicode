d99f456a4f76efb4fcf02c589e2537545f6e2278
Jonathan S Shapiro <jonathan.s.shapiro@gmail.com>
2023-05-02 01:06:22 -0700

    Eliminated extra tail in properties package.

--- property/property.go
+++ property/property.go
@@ -2,6 +2,138 @@ package property
 
 import "unicode"
 
+// Emoji_Modifier_Base: 134 codepoints
+var Emoji_Modifier_Base = &unicode.RangeTable{
+  R16: []unicode.Range16{ /* 2 */
+    unicode.Range16{Lo: 0x261D, Hi: 0x26F9, Stride: 220 },
+    unicode.Range16{Lo: 0x270A, Hi: 0x270D, Stride: 1 },
+  },
+  R32: []unicode.Range32{ /* 32 */
+    unicode.Range32{Lo: 0x01F385, Hi: 0x01F3C2, Stride: 61 },
+    unicode.Range32{Lo: 0x01F3C3, Hi: 0x01F3C4, Stride: 1 },
+    unicode.Range32{Lo: 0x01F3C7, Hi: 0x01F3CA, Stride: 3 },
+    unicode.Range32{Lo: 0x01F3CB, Hi: 0x01F3CC, Stride: 1 },
+    unicode.Range32{Lo: 0x01F442, Hi: 0x01F443, Stride: 1 },
+    unicode.Range32{Lo: 0x01F446, Hi: 0x01F450, Stride: 1 },
+    unicode.Range32{Lo: 0x01F466, Hi: 0x01F478, Stride: 1 },
+    unicode.Range32{Lo: 0x01F47C, Hi: 0x01F481, Stride: 5 },
+    unicode.Range32{Lo: 0x01F482, Hi: 0x01F483, Stride: 1 },
+    unicode.Range32{Lo: 0x01F485, Hi: 0x01F487, Stride: 1 },
+    unicode.Range32{Lo: 0x01F48F, Hi: 0x01F491, Stride: 2 },
+    unicode.Range32{Lo: 0x01F4AA, Hi: 0x01F574, Stride: 202 },
+    unicode.Range32{Lo: 0x01F575, Hi: 0x01F57A, Stride: 5 },
+    unicode.Range32{Lo: 0x01F590, Hi: 0x01F595, Stride: 5 },
+    unicode.Range32{Lo: 0x01F596, Hi: 0x01F645, Stride: 175 },
+    unicode.Range32{Lo: 0x01F646, Hi: 0x01F647, Stride: 1 },
+    unicode.Range32{Lo: 0x01F64B, Hi: 0x01F64F, Stride: 1 },
+    unicode.Range32{Lo: 0x01F6A3, Hi: 0x01F6B4, Stride: 17 },
+    unicode.Range32{Lo: 0x01F6B5, Hi: 0x01F6B6, Stride: 1 },
+    unicode.Range32{Lo: 0x01F6C0, Hi: 0x01F6CC, Stride: 12 },
+    unicode.Range32{Lo: 0x01F90C, Hi: 0x01F90F, Stride: 3 },
+    unicode.Range32{Lo: 0x01F918, Hi: 0x01F91F, Stride: 1 },
+    unicode.Range32{Lo: 0x01F926, Hi: 0x01F930, Stride: 10 },
+    unicode.Range32{Lo: 0x01F931, Hi: 0x01F939, Stride: 1 },
+    unicode.Range32{Lo: 0x01F93C, Hi: 0x01F93E, Stride: 1 },
+    unicode.Range32{Lo: 0x01F977, Hi: 0x01F9B5, Stride: 62 },
+    unicode.Range32{Lo: 0x01F9B6, Hi: 0x01F9B8, Stride: 2 },
+    unicode.Range32{Lo: 0x01F9B9, Hi: 0x01F9BB, Stride: 2 },
+    unicode.Range32{Lo: 0x01F9CD, Hi: 0x01F9CF, Stride: 1 },
+    unicode.Range32{Lo: 0x01F9D1, Hi: 0x01F9DD, Stride: 1 },
+    unicode.Range32{Lo: 0x01FAC3, Hi: 0x01FAC5, Stride: 1 },
+    unicode.Range32{Lo: 0x01FAF0, Hi: 0x01FAF8, Stride: 1 },
+  },
+  LatinOffset: 0,
+}
+
+// Emoji_Component: 146 codepoints
+var Emoji_Component = &unicode.RangeTable{
+  R16: []unicode.Range16{ /* 4 */
+    unicode.Range16{Lo: 0x0023, Hi: 0x002A, Stride: 7 },
+    unicode.Range16{Lo: 0x0030, Hi: 0x0039, Stride: 1 },
+    unicode.Range16{Lo: 0x200D, Hi: 0x20E3, Stride: 214 },
+    unicode.Range16{Lo: 0xFE0F, Hi: 0xFE0F, Stride: 1 },
+  },
+  R32: []unicode.Range32{ /* 4 */
+    unicode.Range32{Lo: 0x01F1E6, Hi: 0x01F1FF, Stride: 1 },
+    unicode.Range32{Lo: 0x01F3FB, Hi: 0x01F3FF, Stride: 1 },
+    unicode.Range32{Lo: 0x01F9B0, Hi: 0x01F9B3, Stride: 1 },
+    unicode.Range32{Lo: 0x0E0020, Hi: 0x0E007F, Stride: 1 },
+  },
+  LatinOffset: 2,
+}
+
+// Extended_Pictographic: 3537 codepoints
+var Extended_Pictographic = &unicode.RangeTable{
+  R16: []unicode.Range16{ /* 36 */
+    unicode.Range16{Lo: 0x00A9, Hi: 0x00AE, Stride: 5 },
+    unicode.Range16{Lo: 0x203C, Hi: 0x2049, Stride: 13 },
+    unicode.Range16{Lo: 0x2122, Hi: 0x2139, Stride: 23 },
+    unicode.Range16{Lo: 0x2194, Hi: 0x2199, Stride: 1 },
+    unicode.Range16{Lo: 0x21A9, Hi: 0x21AA, Stride: 1 },
+    unicode.Range16{Lo: 0x231A, Hi: 0x231B, Stride: 1 },
+    unicode.Range16{Lo: 0x2328, Hi: 0x2388, Stride: 96 },
+    unicode.Range16{Lo: 0x23CF, Hi: 0x23E9, Stride: 26 },
+    unicode.Range16{Lo: 0x23EA, Hi: 0x23F3, Stride: 1 },
+    unicode.Range16{Lo: 0x23F8, Hi: 0x23FA, Stride: 1 },
+    unicode.Range16{Lo: 0x24C2, Hi: 0x25AA, Stride: 232 },
+    unicode.Range16{Lo: 0x25AB, Hi: 0x25B6, Stride: 11 },
+    unicode.Range16{Lo: 0x25C0, Hi: 0x25FB, Stride: 59 },
+    unicode.Range16{Lo: 0x25FC, Hi: 0x25FE, Stride: 1 },
+    unicode.Range16{Lo: 0x2600, Hi: 0x2605, Stride: 1 },
+    unicode.Range16{Lo: 0x2607, Hi: 0x2612, Stride: 1 },
+    unicode.Range16{Lo: 0x2614, Hi: 0x2685, Stride: 1 },
+    unicode.Range16{Lo: 0x2690, Hi: 0x2705, Stride: 1 },
+    unicode.Range16{Lo: 0x2708, Hi: 0x2712, Stride: 1 },
+    unicode.Range16{Lo: 0x2714, Hi: 0x2716, Stride: 2 },
+    unicode.Range16{Lo: 0x271D, Hi: 0x2721, Stride: 4 },
+    unicode.Range16{Lo: 0x2728, Hi: 0x2733, Stride: 11 },
+    unicode.Range16{Lo: 0x2734, Hi: 0x2744, Stride: 16 },
+    unicode.Range16{Lo: 0x2747, Hi: 0x274C, Stride: 5 },
+    unicode.Range16{Lo: 0x274E, Hi: 0x2753, Stride: 5 },
+    unicode.Range16{Lo: 0x2754, Hi: 0x2755, Stride: 1 },
+    unicode.Range16{Lo: 0x2757, Hi: 0x2763, Stride: 12 },
+    unicode.Range16{Lo: 0x2764, Hi: 0x2767, Stride: 1 },
+    unicode.Range16{Lo: 0x2795, Hi: 0x2797, Stride: 1 },
+    unicode.Range16{Lo: 0x27A1, Hi: 0x27BF, Stride: 15 },
+    unicode.Range16{Lo: 0x2934, Hi: 0x2935, Stride: 1 },
+    unicode.Range16{Lo: 0x2B05, Hi: 0x2B07, Stride: 1 },
+    unicode.Range16{Lo: 0x2B1B, Hi: 0x2B1C, Stride: 1 },
+    unicode.Range16{Lo: 0x2B50, Hi: 0x2B55, Stride: 5 },
+    unicode.Range16{Lo: 0x3030, Hi: 0x303D, Stride: 13 },
+    unicode.Range16{Lo: 0x3297, Hi: 0x3299, Stride: 2 },
+  },
+  R32: []unicode.Range32{ /* 27 */
+    unicode.Range32{Lo: 0x01F000, Hi: 0x01F0FF, Stride: 1 },
+    unicode.Range32{Lo: 0x01F10D, Hi: 0x01F10F, Stride: 1 },
+    unicode.Range32{Lo: 0x01F12F, Hi: 0x01F16C, Stride: 61 },
+    unicode.Range32{Lo: 0x01F16D, Hi: 0x01F171, Stride: 1 },
+    unicode.Range32{Lo: 0x01F17E, Hi: 0x01F17F, Stride: 1 },
+    unicode.Range32{Lo: 0x01F18E, Hi: 0x01F191, Stride: 3 },
+    unicode.Range32{Lo: 0x01F192, Hi: 0x01F19A, Stride: 1 },
+    unicode.Range32{Lo: 0x01F1AD, Hi: 0x01F1E5, Stride: 1 },
+    unicode.Range32{Lo: 0x01F201, Hi: 0x01F20F, Stride: 1 },
+    unicode.Range32{Lo: 0x01F21A, Hi: 0x01F22F, Stride: 21 },
+    unicode.Range32{Lo: 0x01F232, Hi: 0x01F23A, Stride: 1 },
+    unicode.Range32{Lo: 0x01F23C, Hi: 0x01F23F, Stride: 1 },
+    unicode.Range32{Lo: 0x01F249, Hi: 0x01F3FA, Stride: 1 },
+    unicode.Range32{Lo: 0x01F400, Hi: 0x01F53D, Stride: 1 },
+    unicode.Range32{Lo: 0x01F546, Hi: 0x01F64F, Stride: 1 },
+    unicode.Range32{Lo: 0x01F680, Hi: 0x01F6FF, Stride: 1 },
+    unicode.Range32{Lo: 0x01F774, Hi: 0x01F77F, Stride: 1 },
+    unicode.Range32{Lo: 0x01F7D5, Hi: 0x01F7FF, Stride: 1 },
+    unicode.Range32{Lo: 0x01F80C, Hi: 0x01F80F, Stride: 1 },
+    unicode.Range32{Lo: 0x01F848, Hi: 0x01F84F, Stride: 1 },
+    unicode.Range32{Lo: 0x01F85A, Hi: 0x01F85F, Stride: 1 },
+    unicode.Range32{Lo: 0x01F888, Hi: 0x01F88F, Stride: 1 },
+    unicode.Range32{Lo: 0x01F8AE, Hi: 0x01F8FF, Stride: 1 },
+    unicode.Range32{Lo: 0x01F90C, Hi: 0x01F93A, Stride: 1 },
+    unicode.Range32{Lo: 0x01F93C, Hi: 0x01F945, Stride: 1 },
+    unicode.Range32{Lo: 0x01F947, Hi: 0x01FAFF, Stride: 1 },
+    unicode.Range32{Lo: 0x01FC00, Hi: 0x01FFFD, Stride: 1 },
+  },
+  LatinOffset: 1,
+}
+
 // Emoji: 1424 codepoints
 var Emoji = &unicode.RangeTable{
   R16: []unicode.Range16{ /* 65 */
@@ -217,181 +349,3 @@ var Emoji_Modifier = &unicode.RangeTable{
   },
   LatinOffset: 0,
 }
-
-// Emoji_Modifier_Base: 134 codepoints
-var Emoji_Modifier_Base = &unicode.RangeTable{
-  R16: []unicode.Range16{ /* 2 */
-    unicode.Range16{Lo: 0x261D, Hi: 0x26F9, Stride: 220 },
-    unicode.Range16{Lo: 0x270A, Hi: 0x270D, Stride: 1 },
-  },
-  R32: []unicode.Range32{ /* 32 */
-    unicode.Range32{Lo: 0x01F385, Hi: 0x01F3C2, Stride: 61 },
-    unicode.Range32{Lo: 0x01F3C3, Hi: 0x01F3C4, Stride: 1 },
-    unicode.Range32{Lo: 0x01F3C7, Hi: 0x01F3CA, Stride: 3 },
-    unicode.Range32{Lo: 0x01F3CB, Hi: 0x01F3CC, Stride: 1 },
-    unicode.Range32{Lo: 0x01F442, Hi: 0x01F443, Stride: 1 },
-    unicode.Range32{Lo: 0x01F446, Hi: 0x01F450, Stride: 1 },
-    unicode.Range32{Lo: 0x01F466, Hi: 0x01F478, Stride: 1 },
-    unicode.Range32{Lo: 0x01F47C, Hi: 0x01F481, Stride: 5 },
-    unicode.Range32{Lo: 0x01F482, Hi: 0x01F483, Stride: 1 },
-    unicode.Range32{Lo: 0x01F485, Hi: 0x01F487, Stride: 1 },
-    unicode.Range32{Lo: 0x01F48F, Hi: 0x01F491, Stride: 2 },
-    unicode.Range32{Lo: 0x01F4AA, Hi: 0x01F574, Stride: 202 },
-    unicode.Range32{Lo: 0x01F575, Hi: 0x01F57A, Stride: 5 },
-    unicode.Range32{Lo: 0x01F590, Hi: 0x01F595, Stride: 5 },
-    unicode.Range32{Lo: 0x01F596, Hi: 0x01F645, Stride: 175 },
-    unicode.Range32{Lo: 0x01F646, Hi: 0x01F647, Stride: 1 },
-    unicode.Range32{Lo: 0x01F64B, Hi: 0x01F64F, Stride: 1 },
-    unicode.Range32{Lo: 0x01F6A3, Hi: 0x01F6B4, Stride: 17 },
-    unicode.Range32{Lo: 0x01F6B5, Hi: 0x01F6B6, Stride: 1 },
-    unicode.Range32{Lo: 0x01F6C0, Hi: 0x01F6CC, Stride: 12 },
-    unicode.Range32{Lo: 0x01F90C, Hi: 0x01F90F, Stride: 3 },
-    unicode.Range32{Lo: 0x01F918, Hi: 0x01F91F, Stride: 1 },
-    unicode.Range32{Lo: 0x01F926, Hi: 0x01F930, Stride: 10 },
-    unicode.Range32{Lo: 0x01F931, Hi: 0x01F939, Stride: 1 },
-    unicode.Range32{Lo: 0x01F93C, Hi: 0x01F93E, Stride: 1 },
-    unicode.Range32{Lo: 0x01F977, Hi: 0x01F9B5, Stride: 62 },
-    unicode.Range32{Lo: 0x01F9B6, Hi: 0x01F9B8, Stride: 2 },
-    unicode.Range32{Lo: 0x01F9B9, Hi: 0x01F9BB, Stride: 2 },
-    unicode.Range32{Lo: 0x01F9CD, Hi: 0x01F9CF, Stride: 1 },
-    unicode.Range32{Lo: 0x01F9D1, Hi: 0x01F9DD, Stride: 1 },
-    unicode.Range32{Lo: 0x01FAC3, Hi: 0x01FAC5, Stride: 1 },
-    unicode.Range32{Lo: 0x01FAF0, Hi: 0x01FAF8, Stride: 1 },
-  },
-  LatinOffset: 0,
-}
-
-// Emoji_Component: 146 codepoints
-var Emoji_Component = &unicode.RangeTable{
-  R16: []unicode.Range16{ /* 4 */
-    unicode.Range16{Lo: 0x0023, Hi: 0x002A, Stride: 7 },
-    unicode.Range16{Lo: 0x0030, Hi: 0x0039, Stride: 1 },
-    unicode.Range16{Lo: 0x200D, Hi: 0x20E3, Stride: 214 },
-    unicode.Range16{Lo: 0xFE0F, Hi: 0xFE0F, Stride: 1 },
-  },
-  R32: []unicode.Range32{ /* 4 */
-    unicode.Range32{Lo: 0x01F1E6, Hi: 0x01F1FF, Stride: 1 },
-    unicode.Range32{Lo: 0x01F3FB, Hi: 0x01F3FF, Stride: 1 },
-    unicode.Range32{Lo: 0x01F9B0, Hi: 0x01F9B3, Stride: 1 },
-    unicode.Range32{Lo: 0x0E0020, Hi: 0x0E007F, Stride: 1 },
-  },
-  LatinOffset: 2,
-}
-
-// Extended_Pictographic: 3537 codepoints
-var Extended_Pictographic = &unicode.RangeTable{
-  R16: []unicode.Range16{ /* 36 */
-    unicode.Range16{Lo: 0x00A9, Hi: 0x00AE, Stride: 5 },
-    unicode.Range16{Lo: 0x203C, Hi: 0x2049, Stride: 13 },
-    unicode.Range16{Lo: 0x2122, Hi: 0x2139, Stride: 23 },
-    unicode.Range16{Lo: 0x2194, Hi: 0x2199, Stride: 1 },
-    unicode.Range16{Lo: 0x21A9, Hi: 0x21AA, Stride: 1 },
-    unicode.Range16{Lo: 0x231A, Hi: 0x231B, Stride: 1 },
-    unicode.Range16{Lo: 0x2328, Hi: 0x2388, Stride: 96 },
-    unicode.Range16{Lo: 0x23CF, Hi: 0x23E9, Stride: 26 },
-    unicode.Range16{Lo: 0x23EA, Hi: 0x23F3, Stride: 1 },
-    unicode.Range16{Lo: 0x23F8, Hi: 0x23FA, Stride: 1 },
-    unicode.Range16{Lo: 0x24C2, Hi: 0x25AA, Stride: 232 },
-    unicode.Range16{Lo: 0x25AB, Hi: 0x25B6, Stride: 11 },
-    unicode.Range16{Lo: 0x25C0, Hi: 0x25FB, Stride: 59 },
-    unicode.Range16{Lo: 0x25FC, Hi: 0x25FE, Stride: 1 },
-    unicode.Range16{Lo: 0x2600, Hi: 0x2605, Stride: 1 },
-    unicode.Range16{Lo: 0x2607, Hi: 0x2612, Stride: 1 },
-    unicode.Range16{Lo: 0x2614, Hi: 0x2685, Stride: 1 },
-    unicode.Range16{Lo: 0x2690, Hi: 0x2705, Stride: 1 },
-    unicode.Range16{Lo: 0x2708, Hi: 0x2712, Stride: 1 },
-    unicode.Range16{Lo: 0x2714, Hi: 0x2716, Stride: 2 },
-    unicode.Range16{Lo: 0x271D, Hi: 0x2721, Stride: 4 },
-    unicode.Range16{Lo: 0x2728, Hi: 0x2733, Stride: 11 },
-    unicode.Range16{Lo: 0x2734, Hi: 0x2744, Stride: 16 },
-    unicode.Range16{Lo: 0x2747, Hi: 0x274C, Stride: 5 },
-    unicode.Range16{Lo: 0x274E, Hi: 0x2753, Stride: 5 },
-    unicode.Range16{Lo: 0x2754, Hi: 0x2755, Stride: 1 },
-    unicode.Range16{Lo: 0x2757, Hi: 0x2763, Stride: 12 },
-    unicode.Range16{Lo: 0x2764, Hi: 0x2767, Stride: 1 },
-    unicode.Range16{Lo: 0x2795, Hi: 0x2797, Stride: 1 },
-    unicode.Range16{Lo: 0x27A1, Hi: 0x27BF, Stride: 15 },
-    unicode.Range16{Lo: 0x2934, Hi: 0x2935, Stride: 1 },
-    unicode.Range16{Lo: 0x2B05, Hi: 0x2B07, Stride: 1 },
-    unicode.Range16{Lo: 0x2B1B, Hi: 0x2B1C, Stride: 1 },
-    unicode.Range16{Lo: 0x2B50, Hi: 0x2B55, Stride: 5 },
-    unicode.Range16{Lo: 0x3030, Hi: 0x303D, Stride: 13 },
-    unicode.Range16{Lo: 0x3297, Hi: 0x3299, Stride: 2 },
-  },
-  R32: []unicode.Range32{ /* 27 */
-    unicode.Range32{Lo: 0x01F000, Hi: 0x01F0FF, Stride: 1 },
-    unicode.Range32{Lo: 0x01F10D, Hi: 0x01F10F, Stride: 1 },
-    unicode.Range32{Lo: 0x01F12F, Hi: 0x01F16C, Stride: 61 },
-    unicode.Range32{Lo: 0x01F16D, Hi: 0x01F171, Stride: 1 },
-    unicode.Range32{Lo: 0x01F17E, Hi: 0x01F17F, Stride: 1 },
-    unicode.Range32{Lo: 0x01F18E, Hi: 0x01F191, Stride: 3 },
-    unicode.Range32{Lo: 0x01F192, Hi: 0x01F19A, Stride: 1 },
-    unicode.Range32{Lo: 0x01F1AD, Hi: 0x01F1E5, Stride: 1 },
-    unicode.Range32{Lo: 0x01F201, Hi: 0x01F20F, Stride: 1 },
-    unicode.Range32{Lo: 0x01F21A, Hi: 0x01F22F, Stride: 21 },
-    unicode.Range32{Lo: 0x01F232, Hi: 0x01F23A, Stride: 1 },
-    unicode.Range32{Lo: 0x01F23C, Hi: 0x01F23F, Stride: 1 },
-    unicode.Range32{Lo: 0x01F249, Hi: 0x01F3FA, Stride: 1 },
-    unicode.Range32{Lo: 0x01F400, Hi: 0x01F53D, Stride: 1 },
-    unicode.Range32{Lo: 0x01F546, Hi: 0x01F64F, Stride: 1 },
-    unicode.Range32{Lo: 0x01F680, Hi: 0x01F6FF, Stride: 1 },
-    unicode.Range32{Lo: 0x01F774, Hi: 0x01F77F, Stride: 1 },
-    unicode.Range32{Lo: 0x01F7D5, Hi: 0x01F7FF, Stride: 1 },
-    unicode.Range32{Lo: 0x01F80C, Hi: 0x01F80F, Stride: 1 },
-    unicode.Range32{Lo: 0x01F848, Hi: 0x01F84F, Stride: 1 },
-    unicode.Range32{Lo: 0x01F85A, Hi: 0x01F85F, Stride: 1 },
-    unicode.Range32{Lo: 0x01F888, Hi: 0x01F88F, Stride: 1 },
-    unicode.Range32{Lo: 0x01F8AE, Hi: 0x01F8FF, Stride: 1 },
-    unicode.Range32{Lo: 0x01F90C, Hi: 0x01F93A, Stride: 1 },
-    unicode.Range32{Lo: 0x01F93C, Hi: 0x01F945, Stride: 1 },
-    unicode.Range32{Lo: 0x01F947, Hi: 0x01FAFF, Stride: 1 },
-    unicode.Range32{Lo: 0x01FC00, Hi: 0x01FFFD, Stride: 1 },
-  },
-  LatinOffset: 1,
-}
-
-// Long names:
-
-var Other = C
-var cntrl = Cc
-var Control = Cc
-var Format = Cf
-var Unassigned = Cn
-var Private_Use = Co
-var Surrogate = Cs
-var Letter = L
-var Cased_Letter = LC
-var Lowercase_Letter = Ll
-var Modifier_Letter = Lm
-var Other_Letter = Lo
-var Titlecase_Letter = Lt
-var Uppercase_Letter = Lu
-var Combining_Mark = M
-var Mark = M
-var Spacing_Mark = Mc
-var Enclosing_Mark = Me
-var Nonspacing_Mark = Mn
-var Number = N
-var digit = Nd
-var Decimal_Number = Nd
-var Letter_Number = Nl
-var Other_Number = No
-var Punctuation = P
-var punct = P
-var Connector_Punctuation = Pc
-var Dash_Punctuation = Pd
-var Close_Punctuation = Pe
-var Final_Punctuation = Pf
-var Initial_Punctuation = Pi
-var Other_Punctuation = Po
-var Open_Punctuation = Ps
-var Symbol = S
-var Currency_Symbol = Sc
-var Modifier_Symbol = Sk
-var Math_Symbol = Sm
-var Other_Symbol = So
-var Separator = Z
-var Line_Separator = Zl
-var Paragraph_Separator = Zp
-var Space_Separator = Zs
-
